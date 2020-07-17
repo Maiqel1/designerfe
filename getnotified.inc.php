60ca2bdd192be59bd582f067aa5576d4b3b9ef02
Maiqel1 <66220194+Maiqel1@users.noreply.github.com>
2020-07-17 11:41:33 +0100

    Update getnotified.inc.php

--- getnotified.inc.php
+++ getnotified.inc.php
@@ -8,7 +8,7 @@ $email = stripslashes($email);
 
 $email = htmlspecialchars($email); // to remove special characters
 
-$result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM diggit WHERE email = '$email';"));
+$result = mysqli_num_rows(mysqli_query($conn, "SELECT 1 FROM diggit2 WHERE email = '$email';"));
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Check if email is valid
 {
@@ -19,7 +19,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Check if email is valid
     $signal = 'bad';
     $message = 'Email already exists';
 } else { // All is good.. Lets save the email
-    $sql = "INSERT INTO diggit (email) VALUES ('$email')";
+    $sql = "INSERT INTO diggit2 (email) VALUES ('$email')";
     
     if (mysqli_query($conn, $sql)) {
         $signal = 'ok';
